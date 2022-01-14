622b48058d0ca7255cf896a0ed5ede5ba43cfe85
hasta <hastashakiba@gmail.com>
2022-01-14 17:46:05 +0330

    add toggle follow api

--- schema.sql
+++ schema.sql
@@ -2,8 +2,22 @@ DROP DATABASE IF EXISTS gocial CASCADE;
 CREATE DATABASE IF NOT EXISTS gocial;
 SET DATABASE = gocial;
 
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL NOT NULL PRIMARY KEY,
-    email VARCHAR NOT NULL UNIQUE,
-    username VARCHAR NOT NULL UNIQUE
+CREATE TABLE IF NOT EXISTS users
+(
+    id              SERIAL  NOT NULL PRIMARY KEY,
+    email           VARCHAR NOT NULL UNIQUE,
+    username        VARCHAR NOT NULL UNIQUE,
+    followers_count INT     NOT NULL DEFAULT 0 CHECK ( followers_count >= 0 ),
+    followees_count INT     NOT NULL DEFAULT 0 CHECK ( followees_count >= 0 )
 );
+
+CREATE TABLE IF NOT EXISTS follows
+(
+    follower_id INT NOT NULL,
+    followee_id INT NOT NULL,
+    PRIMARY KEY (follower_id, followee_id)
+);
+
+INSERT INTO users (id, email, username)
+VALUES (1, 'john@example.org', 'john'),
+       (2, 'jane@example.org', 'jane');
